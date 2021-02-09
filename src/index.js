ee96a96676b7cd6a0577fa79604817ee2a87bdeb
Ameen Alade <alade.ameen@venturegardengroup.com>
2021-02-09 16:02:04 +0100

    Added routing to the app using react router

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import './index.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+  
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>    
   </React.StrictMode>,
   document.getElementById('root')
 );
